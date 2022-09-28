6307dbe28f8e6e7e290fdaccd1f2851fc0bff590
danieltorredlopem <danieltorredelope@gmail.com>
2022-09-28 08:48:26 +0200

    modified

---
+++ Ejercicios js/ejercicioCadenas.js
@@ -0,0 +1,4 @@
+var daw = "Desarrollo de aplicaciones web";
+var dam = "Desarrollo de aplicaciones multiplataforma";
+var asir = "Administración de 'Sistemas Informáticos' en red";
+var smr = "Sistemas \"Microinformáticos\" y redes";
