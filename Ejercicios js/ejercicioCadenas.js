314bba43800a503fe14b03dead52467f72db66de
danieltorredlopem <danieltorredelope@gmail.com>
2022-09-28 08:53:26 +0200

    modified

--- Ejercicios js/ejercicioCadenas.js
+++ Ejercicios js/ejercicioCadenas.js
@@ -4,3 +4,8 @@ var asir = "Administración de 'Sistemas Informáticos' en red";
 var smr = "Sistemas \"Microinformáticos\" y redes";
 
 var ciclo = new String("");
+
+ciclo += "Hay 3 ciclos formativos de grado superior\n";
+ciclo += dam+", "+daw+" y "+asir;
+
+alert(ciclo);
