acc1b93aef1d7aaf5394572a878b565fe98de58a
danieltorredlopem <danieltorredelope@gmail.com>
2022-09-29 09:47:35 +0200

    modified

--- Ejercicios js/ejercicioCine.js
+++ Ejercicios js/ejercicioCine.js
@@ -5,6 +5,7 @@ alert(typeof (nombreCliente));
 while (typeof (nombreCliente) != 'string' || typeof (nombreCliente) == null) {
     let nuevoNombreCliente = prompt("Ha indicado un valor erróneo para el campo nombre, vuelva a intentarlo, por favor");
     nombreCliente = nuevoNombreCliente;
+    alert(nombreCliente);
 }
 
 //1. comprobamos que la edad del cliente sea un numero y que no sea nulo, si no es así insistimos
@@ -12,6 +13,55 @@ let edadCliente = parseInt(prompt("Dígame su edad, por favor"));
 alert(typeof (edadCliente));
 
 while (typeof(edadCliente) != 'number' || typeof(edadCliente) == null) {
-    let nuevaEdadCliente = prompt("Ha indicado un valor erróneo para el campo edad, vuelva a intentarlo, por favor");
+    let nuevaEdadCliente = parseInt(prompt("Ha indicado un valor erróneo para el campo edad, vuelva a intentarlo, por favor"));
     edadCliente = nuevaEdadCliente;
+    alert(edadCliente);
 }
+
+let peliculas = ["Spider Man","Superman","Dragon Ball Super","La era del hielo","18videos"];
+alert("Las peliculas disponibles son "+peliculas);
+peliculaCliente = prompt("¿Que película desea ver?");
+
+ let peliculaEncontrada;
+
+ if (edadCliente <= 17 && peliculaCliente == "18videos") {
+    while (peliculaCliente == "18videos") {
+        peliculaCliente = prompt("No tiene edad suficiente para ver esa película, por favor seleccione otra");
+        alert(peliculaCliente);
+    }
+ }
+for(i=0;i<peliculas.length;i++){
+    if (peliculaCliente === peliculas[i]) {
+        peliculaEncontrada = true;
+    }
+}
+
+if (peliculaEncontrada == true) {
+    alert("La pelicula "+peliculaCliente+" se encuentra en el catálogo");
+} else {
+    alert("La pelicula "+peliculaCliente+" no se encuentra en el catálogo");
+}
+
+
+let comidaCliente;
+let descuentoCliente;
+
+if (edadCliente <= 17) {
+    comidaCliente = "caramelos y refresco";
+    descuentoCliente = "Descuento del 50%";
+} else if (edadCliente >= 18 && edadCliente <= 25) {
+    comidaCliente = "Palomitas, perrito y refresco";
+    descuentoCliente = "No hay descuento";
+} else if (edadCliente >= 26 && edadCliente <= 35) {
+    comidaCliente = "Perrito y refresco";
+    descuentoCliente = "Descuento del 10%";
+} else if(edadCliente >= 36){
+    comidaCliente = "Agua";
+    descuentoCliente = "Descuento del 20%";
+}
+
+console.log("Nombre: "+nombreCliente);
+console.log("Edad: "+edadCliente);
+console.log("Película"+peliculaCliente);
+console.log("Comida: "+comidaCliente);
+console.log("Descuento: "+descuentoCliente);
