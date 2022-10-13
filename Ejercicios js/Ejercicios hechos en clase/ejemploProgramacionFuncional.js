782141d80e0a86f0f77a63eb7df1c47c7714c618
danieltorredlopem <danieltorredelope@gmail.com>
2022-10-13 08:31:56 +0200

    modified

---
+++ Ejercicios js/Ejercicios hechos en clase/ejemploProgramacionFuncional.js
@@ -0,0 +1,10 @@
+let miArrayNotas = [5.4, 5, 3, 2, 2.4, 6, 6, 9];
+let miArraydeAprobados=[];
+
+for (let i = 0; i < miArrayNotas.length; i++) {
+    let nota = miArrayNotas[i];
+    
+    if (nota >= 5) {
+        miArraydeAprobados.push[nota];
+    }  
+}
