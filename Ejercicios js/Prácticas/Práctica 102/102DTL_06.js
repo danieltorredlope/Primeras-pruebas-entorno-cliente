040a77b6b81065fd90ea9d5b828e843c683b4538
danieltorredlopem <danieltorredelope@gmail.com>
2022-10-24 10:16:14 +0200

    modified

---
+++ Ejercicios js/Prácticas/Práctica 102/102DTL_06.js
@@ -0,0 +1,16 @@
+/*
+Trata de organizar el código de tu programa (¿quitando switch?) usando funciones.
+ Define y llama a tres funciones en los lugares apropiados:
+
+⦁	showContact: la función debe tomar dos argumentos; el primero es la lista de contactos y
+    el segundo es el número de índice del contacto a mostrar; dentro de la función,
+    verifica si se pasan los argumentos correctos, es decir, si los contactos son un array
+   (utiliza la construcción instanceofArray para esto).
+
+⦁	showAllContacts: la función debe tomar un argumento, la lista de contactos; dentro de la función,
+     verifica si el argumento dado es un array.
+
+⦁	addNewContact: la función debe tomar cuatro argumentos, una lista de contactos
+    y los datos del nuevo contacto, es decir: nombre, teléfono y número; antes de agregar
+    un nuevo contacto, verifica si el argumento pasado es un array y si los datos del nuevo contacto tienen algún valor.
+*/
