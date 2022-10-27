cb3716d6f23524e4832fcac2065525c078cd79f3
danieltorredlopem <danieltorredelope@gmail.com>
2022-10-27 08:51:34 +0200

    modified

--- Scripts js/Primeros programas/operadorTernario.js
+++ Scripts js/Primeros programas/operadorTernario.js
@@ -3,4 +3,7 @@ let edad=22;
 let puedeVotar = (edad>18) ? "Puede votar" : "No puede votar";
 alert(puedeVotar);
 
+let numero = prompt("dame un numero");
+
+
 //Este tipo de operación sería lo mismo que un IF (edad>18), THEN (Puede votar), ELSE (No puede votar)
